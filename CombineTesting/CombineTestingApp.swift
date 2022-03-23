06a169af70d3197489fde63e495493920329072c
Federico <codepalace@fastmail.se>
2022-03-23 21:32:23 +0100

    Initial Commit

---
+++ CombineTesting/CombineTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineTestingApp.swift
+//  CombineTesting
+//
+//  Created by Federico on 23/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CombineTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CombineTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CombineTesting
+//
+//  Created by Federico on 23/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
